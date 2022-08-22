9780cdfd14a46f1dd2dc3f2f006ebe6f435917dd
Archerzqy <zqy_Archer@163.com>
2022-08-22 10:26:06 +0800

    添加@Mapper

--- src/main/java/com/blog/mapper/UploadMapper.java
+++ src/main/java/com/blog/mapper/UploadMapper.java
@@ -1,9 +1,10 @@
 package com.blog.mapper;
 
 import com.blog.po.User;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
-
+@Mapper
 @Repository
 public interface UploadMapper {
 
